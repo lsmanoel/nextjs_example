90cf033cbe5a4e78cdad01a31e8e5da1e53cff0a
Lucas Seara Manoel <lucas.manoel@khomp.com>
2022-06-24 14:41:15 -0300

    chore: add page extensions

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  pageExtensions: [
+    "page.mdx",
+    "page.md",
+    "page.jsx",
+    "page.js",
+    "page.tsx",
+    "page.ts",
+  ],
   env: {
     SERVICE_ID: process.env.SERVICE_ID,
     TEMPLATE_ID: process.env.TEMPLATE_ID,
