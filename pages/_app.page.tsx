85ba3364d90a2ca3cad864b296804e80f4131517
Lucas Seara Manoel <lucas.manoel@khomp.com>
2022-07-03 19:17:07 -0300

    fix: test .lintstagedrd

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -5,7 +5,7 @@ import { SessionProvider } from "next-auth/react";
 
 import "styles/globals.scss";
 
-test lint-staged fxdsxx
+test lint-staged fxds
 
 function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [layout, setLayout] = useState<boolean>(true);
