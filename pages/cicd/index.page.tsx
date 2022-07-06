4760472d9109273c3f8bcce4448e0193e258373f
Lucas Seara Manoel <lucas.manoel@khomp.com>
2022-07-06 10:12:54 -0300

    chore: add react-code-blocks

--- pages/cicd/index.page.tsx
+++ pages/cicd/index.page.tsx
@@ -1,7 +1,22 @@
 import { NextPage } from "next";
-import styles from "styles/pages/Page.module.scss";
+import { CodeBlock, dracula } from "react-code-blocks";
 import Head from "next/head";
+import styles from "styles/pages/Page.module.scss";
+import { useEffect, useState } from "react";
+
 const CICD: NextPage = () => {
+  const [code, setCode] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  fetch(
+    "https://raw.githubusercontent.com/lsmanoel/nextjs_example/main/.github/workflows/github-actions-CI.yml"
+  ).then((r) => {
+    r.text().then((d) => setCode(d));
+  });
+
+  useEffect(() => {
+    code != "" && setLoading(false);
+  }, [code]);
+
   return (
     <>
       <Head>
@@ -12,6 +27,20 @@ const CICD: NextPage = () => {
           <div className={styles.lightBox}>
             <h1> CI/CD </h1>
           </div>
+
+          {!loading && (
+            <div className={styles.CodeBlock}>
+              <CodeBlock
+                text={code}
+                language="yml"
+                showLineNumbers={true}
+                theme={dracula}
+                customStyle={{
+                  background: "none",
+                }}
+              />
+            </div>
+          )}
         </main>
       </div>
     </>
