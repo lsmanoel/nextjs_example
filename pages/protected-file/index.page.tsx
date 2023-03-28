d6357dd6c6c38947a49ed659535bf6f4b36e8cac
Lucas Seara Manoel <ls.manoel@gmail.com>
2023-03-28 13:27:33 -0300

    feat: add protected content

--- pages/protected-file/index.page.tsx
+++ pages/protected-file/index.page.tsx
@@ -42,25 +42,49 @@ const ProtectedFile: NextPage = () => {
 
           <div className={styles.column}>
             <h2>Sobre o documento protegido</h2>
+            <h3>Olá {name}. Você recebeu acesso ao conteúdo protegido</h3>
             <h3>
-              Esse site foi implementado com o framework{" "}
+              Uma vez que esse site foi implementado com o framework{" "}
               <a href="https://nextjs.org/" target="_blank">
                 NextJs
               </a>{" "}
-              que foi feito em cima do biblioteca{" "}
-              <a href="https://react.dev/" target="_blank">
-                ReactJs
+              ele possui o recurso que possibilita a renderização da página no
+              lado do servidor, e assim enviar para o navegador uma página
+              estática. O nome desse recurso é{" "}
+              <a
+                href="https://www.adservio.fr/post/server-side-rendering-advantages#:~:text=With%20server%2Dside%20rendering%2C%20even,data%20privacy%20and%20ensuring%20compliance."
+                target="_blank"
+              >
+                ServerSideRendering
               </a>
-              . Para a estilização foi utilizado o{" "}
-              <a href="https://sass-lang.com/" target="_blank">
-                Sass
-              </a>{" "}
-              e para os recursos de login foi utilizado o{" "}
-              <a href="https://next-auth.js.org/" target="_blank">
-                NextAuth
-              </a>
-              .
+              . Logo sem a efetuação do Login esse conteúdo jamais seria mandado
+              para o navegador do cliente. Essa página serve como demonstração
+              desse recurso. Como você efetuou o login, você recebeu acesso ao
+              conteúdo secreto e protegido:
             </h3>
+          </div>
+          <div>
+            <h2>Receita de Miojo</h2>
+            <ul>
+              <li>
+                <p>1. Coloque o fogo para ferver.</p>
+              </li>
+              <li>
+                <p>2. Em seguida coloque as 4 colheres de catchup e mexa.</p>
+              </li>
+              <li>
+                <p>3. Depois quando a água estiver fervendo ponhe o miojo.</p>
+              </li>
+              <li>
+                <p>4. Em seguida coloque o tempero e as colheres de pimenta.</p>
+              </li>
+              <li>
+                <p>5. Depois rale a mussarela em cima do miojo.</p>
+              </li>
+              <li>
+                <p>6. Bom apetite!</p>
+              </li>
+            </ul>
           </div>
         </main>
       </div>
