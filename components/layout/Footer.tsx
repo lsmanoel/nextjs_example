09cbcf853d244bd4d371279d2df1870ca223e320
Lucas Seara Manoel <ls.manoel@gmail.com>
2023-03-31 14:59:17 -0300

    refactor: set updates date

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,5 +1,5 @@
 import { ReactElement } from "react";
 
 export default function Footer(): ReactElement {
-  return <footer>Footer</footer>;
+  return <footer>Última Atualização: 31/03/2023</footer>;
 }
