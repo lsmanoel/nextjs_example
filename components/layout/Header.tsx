de8f09d371747e79eee2ca6984eedc63a6dff79c
Lucas Seara Manoel <ls.manoel@gmail.com>
2023-04-03 10:23:49 -0300

    @tippyjs/react

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -2,7 +2,8 @@ import React, { ReactElement, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
-import ReactTooltip from "react-tooltip";
+import Tippy from "@tippyjs/react";
+import "tippy.js/dist/tippy.css";
 
 import { statusColor } from "lib/statusColor";
 
@@ -34,45 +35,53 @@ export default function Header({
       <button
         className={`${buttonFaBarsEnable ? styles.buttonEnable : ""}`}
         aria-label="Esconder menu lateral"
-        title="Esconder menu lateral"
         onClick={() => onClickFaBars()}
       >
         <FontAwesomeIcon icon={faBars}></FontAwesomeIcon>
       </button>
       <div>
         {session?.user?.image && (
-          <button className={styles.avatar} onClick={() => onClickAvatar()}>
-            <Image
-              alt="avatar"
-              src={session.user.image}
-              width={38}
-              height={38}
-            />
-          </button>
+          <Tippy content={"Página de Autenticação"}>
+            <button className={styles.avatar} onClick={() => onClickAvatar()}>
+              <Image
+                alt="avatar"
+                src={session.user.image}
+                width={38}
+                height={38}
+              />
+            </button>
+          </Tippy>
         )}
-        <button
-          className={styles.iconButton}
-          type="button"
-          onClick={(e) => {
-            e.preventDefault();
-            window.open("https://github.com/lsmanoel/nextjs_example", "_blank");
-          }}
-        >
-          <FontAwesomeIcon icon={faGithub} size="xl"></FontAwesomeIcon>
-        </button>
 
-        <button
-          className={`${buttonMsgEnable ? styles.buttonEnable : ""}
-                    ${submitMsgStatus === "success" ? styles.success : ""}
-                    ${submitMsgStatus === "warn" ? styles.warn : ""}
-                    ${submitMsgStatus === "error" ? styles.error : ""}`}
-          aria-label="Esconder Messenger"
-          title="Esconder Messenger"
-          onClick={() => onClickMsg()}
-        >
-          <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
-          <span>Envie uma mensagem</span>
-        </button>
+        <Tippy content={"Link para o projeto"}>
+          <button
+            className={styles.iconButton}
+            type="button"
+            onClick={(e) => {
+              e.preventDefault();
+              window.open(
+                "https://github.com/lsmanoel/nextjs_example",
+                "_blank"
+              );
+            }}
+          >
+            <FontAwesomeIcon icon={faGithub} size="xl"></FontAwesomeIcon>
+          </button>
+        </Tippy>
+
+        <Tippy content={"Eenvie uma mensagem para o Lucas"}>
+          <button
+            className={`${buttonMsgEnable ? styles.buttonEnable : ""}
+                      ${submitMsgStatus === "success" ? styles.success : ""}
+                      ${submitMsgStatus === "warn" ? styles.warn : ""}
+                      ${submitMsgStatus === "error" ? styles.error : ""}`}
+            aria-label="Esconder Messenger"
+            onClick={() => onClickMsg()}
+          >
+            <FontAwesomeIcon icon={faPaperPlane}></FontAwesomeIcon>
+            <span>Envie uma mensagem</span>
+          </button>
+        </Tippy>
       </div>
     </header>
   );
