e2be4991245a9b7c0581205a694340232a7e2e30
Lucas Seara Manoel <ls.manoel@gmail.com>
2023-04-10 11:11:24 -0300

    fix: show content after width size is load

--- pages/cicd/index.page.tsx
+++ pages/cicd/index.page.tsx
@@ -11,8 +11,8 @@ const CICD: NextPage = () => {
   const [innerWidth, getInnerWidth] = useState(innerWidthThreshold + 1);
   const [code, setCode] = useState<string>("");
   const [loading, setLoading] = useState<boolean>(true);
+  const [loadingWidth, setLoadingWidth] = useState(true);
   const setInnerWidth = () => {
-    console.log(window.innerWidth);
     getInnerWidth(window.innerWidth);
   };
 
@@ -20,6 +20,7 @@ const CICD: NextPage = () => {
     code != "" && setLoading(false);
   }, [code]);
   useEffect(() => {
+    setLoadingWidth(false);
     setInnerWidth();
   }, []);
   useEffect(() => {
@@ -47,74 +48,76 @@ const CICD: NextPage = () => {
           mobileWidthThreshold > innerWidth && styles.containerMobile
         }`}
       >
-        <main className={styles.main}>
-          <div className={styles.lightBox}>
-            <h1> CI/CD </h1>
-          </div>
+        {~loadingWidth && (
+          <main className={styles.main}>
+            <div className={styles.lightBox}>
+              <h1> CI/CD </h1>
+            </div>
 
-          <div
-            className={
-              innerWidth > innerWidthThreshold ? styles.row : styles.column
-            }
-          >
-            <div className={styles.column}>
-              <h2>Pipeline</h2>
-              <ul>
-                <li>
-                  <p>
-                    1. Teste com{" "}
-                    <a
-                      href="https://github.com/bats-core/bats-core#readme"
-                      target="_blank"
-                    >
-                      Bats
-                    </a>
-                    .
-                  </p>
-                </li>
-                <li>
-                  <p>2. Build do projeto.</p>
-                </li>
-                <li>
-                  <p>3. Testes unitário.</p>
-                </li>
-              </ul>
-            </div>
-            <div className={styles.column}>
-              {!loading && (
-                <div
-                  className={`${styles.CodeBlock} ${
-                    mobileWidthThreshold > innerWidth &&
-                    styles.mobileDivCodeBlock
-                  }`}
-                >
-                  <div>
-                    <a
-                      href="https://github.com/lsmanoel/nextjs_example/blob/main/.github/workflows/github-actions-CI.yml"
-                      target="_blank"
-                    >
-                      .github/workflows/github-actions-CI.yml
-                    </a>
-                    <BuildStatusBadge>
-                      [![github-actions-ci](https://github.com/lsmanoel/curriculum/actions/workflows/github-actions-CI.yml/badge.svg)](https://github.com/lsmanoel/curriculum/actions/workflows/github-actions-CI.yml)
-                    </BuildStatusBadge>
+            <div
+              className={
+                innerWidth > innerWidthThreshold ? styles.row : styles.column
+              }
+            >
+              <div className={styles.column}>
+                <h2>Pipeline</h2>
+                <ul>
+                  <li>
+                    <p>
+                      1. Teste com{" "}
+                      <a
+                        href="https://github.com/bats-core/bats-core#readme"
+                        target="_blank"
+                      >
+                        Bats
+                      </a>
+                      .
+                    </p>
+                  </li>
+                  <li>
+                    <p>2. Build do projeto.</p>
+                  </li>
+                  <li>
+                    <p>3. Testes unitário.</p>
+                  </li>
+                </ul>
+              </div>
+              <div className={styles.column}>
+                {!loading && (
+                  <div
+                    className={`${styles.CodeBlock} ${
+                      mobileWidthThreshold > innerWidth &&
+                      styles.mobileDivCodeBlock
+                    }`}
+                  >
+                    <div>
+                      <a
+                        href="https://github.com/lsmanoel/nextjs_example/blob/main/.github/workflows/github-actions-CI.yml"
+                        target="_blank"
+                      >
+                        .github/workflows/github-actions-CI.yml
+                      </a>
+                      <BuildStatusBadge>
+                        [![github-actions-ci](https://github.com/lsmanoel/curriculum/actions/workflows/github-actions-CI.yml/badge.svg)](https://github.com/lsmanoel/curriculum/actions/workflows/github-actions-CI.yml)
+                      </BuildStatusBadge>
+                    </div>
+                    {mobileWidthThreshold < innerWidth && (
+                      <CodeBlock
+                        text={code}
+                        language="yml"
+                        showLineNumbers={true}
+                        theme={dracula}
+                        customStyle={{
+                          background: "none",
+                        }}
+                      />
+                    )}
                   </div>
-                  {mobileWidthThreshold < innerWidth && (
-                    <CodeBlock
-                      text={code}
-                      language="yml"
-                      showLineNumbers={true}
-                      theme={dracula}
-                      customStyle={{
-                        background: "none",
-                      }}
-                    />
-                  )}
-                </div>
-              )}
+                )}
+              </div>
             </div>
-          </div>
-        </main>
+          </main>
+        )}
       </div>
     </>
   );
--- pages/index.page.tsx
+++ pages/index.page.tsx
@@ -7,12 +7,14 @@ const Home: NextPage = () => {
   const innerWidthThreshold = 1400;
   const mobileWidthThreshold = 800;
   const [innerWidth, getInnerWidth] = useState(innerWidthThreshold + 1);
+  const [loadingWidth, setLoadingWidth] = useState(true);
   const setInnerWidth = () => {
     console.log(window.innerWidth);
     getInnerWidth(window.innerWidth);
   };
 
   useEffect(() => {
+    setLoadingWidth(false);
     setInnerWidth();
   }, []);
   useEffect(() => {
@@ -32,107 +34,109 @@ const Home: NextPage = () => {
           mobileWidthThreshold > innerWidth && styles.containerMobile
         }`}
       >
-        <main className={styles.main}>
-          <div className={styles.lightBox}>
-            <h1> Lucas Seara Manoel </h1>
-          </div>
+        {~loadingWidth && (
+          <main className={styles.main}>
+            <div className={styles.lightBox}>
+              <h1> Lucas Seara Manoel </h1>
+            </div>
 
-          <div
-            className={
-              innerWidth > innerWidthThreshold ? styles.row : styles.column
-            }
-          >
-            <div className={styles.column}>
-              <h2>Resumo </h2>
-              <h3>
-                Durante a graduação (Engenharia Eletrônica) tive o contato com
-                diferentes linguagens de programação, incluindo Python para
-                computação científica e processamento de vídeo (OpenCV). Também
-                já desenvolvi firmwares utilizando FreeRTOS. Sempre me
-                interessei por Inteligência Artificial. Fora da graduação fiz
-                cursos de TensorFlow, Keras e Dlib. Desenvolvi um jogo baseado
-                em AI chamado{" "}
-                <a
-                  href="https://github.com/lsmanoel/MachineRevolution"
-                  target="_blank"
-                >
-                  Machine Revolution
-                </a>{" "}
-                . No último semestre da graduação, auxiliei a tese de doutorado
-                de um professor utilizando Python, Pandas e Matplotlib para
-                processar dados provenientes de sensores e apresentar os
-                resultados desse processamento. Nesse mesmo semestre, eu e mais
-                três colegas, ganhamos o prêmio do Desafio Ifsc Ideias
-                Inovadoras. A ideia consistia de um sistema baseado em visão
-                computacional para análise de tráfego de veículos e pedestres. O
-                objetivo era, por meio dos dados adquiridos, descobrir se há
-                falta ou excesso na iluminação pública. A ideia também consistia
-                em diminuir ou aumentar a iluminação de acordo com a demanda.
-                Meu TCC foi sobre Visão Computacional, utilizei Python, C++ e
-                Linux embarcado com o sistema ROS (Robot Operating System).
-                Segue o{" "}
-                <a
-                  href="https://drive.google.com/file/d/1Ei1-eol5fQ9zp7CFsud-u0lzOeMOkwBS/view?usp=sharing"
-                  target="_blank"
-                >
-                  link do TCC
-                </a>{" "}
-                . Após o término da graduação trabalhei como freelancer no
-                firmware de um motor de popa elétrico controlado remotamente que
-                utiliza o microcontrolador Stm32f103c8t6 e o rádio RF24. Também
-                trabalhei na criação de aplicativos WEB e Mobile. Trabalhei como
-                programador fullstack freelancer utilizando ReactJS, React
-                Native, NodeJS e MongoDB. Também já trabalhei com NextJS e
-                deploy em Kubernets na Azure. A maior parte dos meus trabalhos
-                foram com frontend utilizando TypeScript, mas tenho interesse em
-                migrar para trabalhar com backend e data science utilizando
-                Python.
-              </h3>
+            <div
+              className={
+                innerWidth > innerWidthThreshold ? styles.row : styles.column
+              }
+            >
+              <div className={styles.column}>
+                <h2>Resumo </h2>
+                <h3>
+                  Durante a graduação (Engenharia Eletrônica) tive o contato com
+                  diferentes linguagens de programação, incluindo Python para
+                  computação científica e processamento de vídeo (OpenCV).
+                  Também já desenvolvi firmwares utilizando FreeRTOS. Sempre me
+                  interessei por Inteligência Artificial. Fora da graduação fiz
+                  cursos de TensorFlow, Keras e Dlib. Desenvolvi um jogo baseado
+                  em AI chamado{" "}
+                  <a
+                    href="https://github.com/lsmanoel/MachineRevolution"
+                    target="_blank"
+                  >
+                    Machine Revolution
+                  </a>{" "}
+                  . No último semestre da graduação, auxiliei a tese de
+                  doutorado de um professor utilizando Python, Pandas e
+                  Matplotlib para processar dados provenientes de sensores e
+                  apresentar os resultados desse processamento. Nesse mesmo
+                  semestre, eu e mais três colegas, ganhamos o prêmio do Desafio
+                  Ifsc Ideias Inovadoras. A ideia consistia de um sistema
+                  baseado em visão computacional para análise de tráfego de
+                  veículos e pedestres. O objetivo era, por meio dos dados
+                  adquiridos, descobrir se há falta ou excesso na iluminação
+                  pública. A ideia também consistia em diminuir ou aumentar a
+                  iluminação de acordo com a demanda. Meu TCC foi sobre Visão
+                  Computacional, utilizei Python, C++ e Linux embarcado com o
+                  sistema ROS (Robot Operating System). Segue o{" "}
+                  <a
+                    href="https://drive.google.com/file/d/1Ei1-eol5fQ9zp7CFsud-u0lzOeMOkwBS/view?usp=sharing"
+                    target="_blank"
+                  >
+                    link do TCC
+                  </a>{" "}
+                  . Após o término da graduação trabalhei como freelancer no
+                  firmware de um motor de popa elétrico controlado remotamente
+                  que utiliza o microcontrolador Stm32f103c8t6 e o rádio RF24.
+                  Também trabalhei na criação de aplicativos WEB e Mobile.
+                  Trabalhei como programador fullstack freelancer utilizando
+                  ReactJS, React Native, NodeJS e MongoDB. Também já trabalhei
+                  com NextJS e deploy em Kubernets na Azure. A maior parte dos
+                  meus trabalhos foram com frontend utilizando TypeScript, mas
+                  tenho interesse em migrar para trabalhar com backend e data
+                  science utilizando Python.
+                </h3>
+              </div>
+              <div className={styles.column}>
+                <h2>Conhecimentos de Programação</h2>
+                <ul>
+                  <li>
+                    <p>
+                      <b>Fullstack Dev</b> (JavaScript, TypeScript, CSS, SQL,
+                      NextJS, ReactJS, React Native, NodeJS, MongoDB, NeDB,
+                      Firebase, Postgres)
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <b>AI</b> (Python: Keras, Tensorflow 1, DLib)
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <b>ROS</b> (UNIX, C++, Python, cmake)
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <b>OpenCV</b> (C++, Python)
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <b>Programação de Hardware</b> (VHDL)
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <b>Jogos e Animações</b> (Unity, P5, Processing, PyGame)
+                    </p>
+                  </li>
+                  <li>
+                    <p>
+                      <b>Firmwares</b> (C, C++, Assembly, FreeRTOS)
+                    </p>
+                  </li>
+                </ul>
+              </div>
             </div>
-            <div className={styles.column}>
-              <h2>Conhecimentos de Programação</h2>
-              <ul>
-                <li>
-                  <p>
-                    <b>Fullstack Dev</b> (JavaScript, TypeScript, CSS, SQL,
-                    NextJS, ReactJS, React Native, NodeJS, MongoDB, NeDB,
-                    Firebase, Postgres)
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <b>AI</b> (Python: Keras, Tensorflow 1, DLib)
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <b>ROS</b> (UNIX, C++, Python, cmake)
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <b>OpenCV</b> (C++, Python)
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <b>Programação de Hardware</b> (VHDL)
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <b>Jogos e Animações</b> (Unity, P5, Processing, PyGame)
-                  </p>
-                </li>
-                <li>
-                  <p>
-                    <b>Firmwares</b> (C, C++, Assembly, FreeRTOS)
-                  </p>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </main>
+          </main>
+        )}
       </div>
     </>
   );
