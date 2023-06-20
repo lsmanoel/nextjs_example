888804cbc77c20ea3577b3132261fcf32eb41e5c
Lucas Seara Manoel <ls.manoel@gmail.com>
2023-06-20 19:13:17 -0300

    feat: add firebase init

--- lib/firebase/index.ts
+++ lib/firebase/index.ts
@@ -1,13 +1,26 @@
-import admin, { ServiceAccount } from "firebase-admin";
-import serviceAccount from "./service-account-key.json";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-if (!admin.apps.length) {
-  try {
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount as ServiceAccount),
-    });
-  } catch (error) {
-    console.log("Firebase admin initialization error", error);
-  }
-}
-export default admin.firestore();
+// const firebaseConfig = {
+//   apiKey: process.env.apiKey,
+//   authDomain: process.env.authDomain,
+//   projectId: process.env.projectId,
+//   storageBucket: process.env.storageBucket,
+//   messagingSenderId: process.env.messagingSenderId,
+//   appId: process.env.appId,
+//   measurementId: process.env.measurementId,
+// };
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDIgpo8IS5z-jSwtR6dgyZt0r17f_f8hI",
+  authDomain: "chat-database-curriculum.firebaseapp.com",
+  projectId: "chat-database-curriculum",
+  storageBucket: "chat-database-curriculum.appspot.com",
+  messagingSenderId: "755418524068",
+  appId: "1:755418524068:web:511feedbfe7b23e936f931",
+  measurementId: "G-DGN4F77SRF",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
